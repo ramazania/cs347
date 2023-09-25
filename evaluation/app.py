73767a345fcc894b71ae0ea76cef99865a63e844
ramazania <ramazania@LAPTOP-F7UE5P2T>
2023-09-24 19:52:52 -0500

    added the evaluation folder

---
+++ evaluation/app.py
@@ -0,0 +1,14 @@
+import requests
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    response = requests.get('http://api:5000/newgame/x')
+    jsonResponse = response.json()    
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(port=5000)
